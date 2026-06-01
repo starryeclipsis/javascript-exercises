a054195c0039e5966d01653edf663c4936e23e9f
starryeclipsis <starry_eclipsis@hotmail.com>
2026-06-01 13:23:08 +0000

    06 completed

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str,num) {
+    let str2 = "";
+    if (num < 0){
+        return "ERROR";
+    }
+    for (let i = 0; i < num; i++){
+        str2 += str;
+    }
+    return str2;
 };
 
 // Do not edit below this line
